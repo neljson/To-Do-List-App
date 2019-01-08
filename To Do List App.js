2e55443b4075f7113e04c9d96cfe446af655b9a9
Nelson Wu <nwu@email.wm.edu>
2019-01-07 23:39:42 -0500

    Version 4

--- To Do List App.js
+++ To Do List App.js
@@ -1,20 +1,30 @@
-// Version 2 and 3 (objects): It should oragnize functions on a Todo list object
+// Version 4 (adding items to arrays as objects): It should have objects as an array instead of strings in order to represent more data, 
+// ie whether we completed the task
 
 const todo = {
-    todo: ['item1', 'item2', 'item3'],
+    todo: [],
     displayTodo: function () {
         console.log(this.todo)
     },
-    addTodo: function(newItem) {
-        this.todo.push(newItem)
+    addTodo: function(todoText) {
+        this.todo.push({
+            todoText: todoText,
+            completed: false
+        })
         this.displayTodo ()
     },
-    changeTodo: function(position, newValue) {
-        this.todo[position] = newValue
+    changeTodo: function(position, todoText) {
+        this.todo[position].todoText = todoText
         this.displayTodo()
     },
     deleteTodo: function(position) {
         this.todo.splice(position, 1)
         this.displayTodo()
-    }
+    },
+    toggleCompleted: function(position) {
+        const todo = this.todo[position]
+        todo.completed = !todo.completed
+        this.displayTodo()
+    },
+
 }
